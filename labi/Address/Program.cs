e9caeae6849a52634ddd348042c78388a87fcb75
Arab645 <yasinski0202@gmail.com>
2023-10-01 12:24:14 +0300

    hw

---
+++ labi/Address/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hwForUniversity
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Address myAddress = new Address();
+            
+            myAddress.Index = 1234;
+            myAddress.Country = "Ukraine";
+            myAddress.City = "Kyiv";
+            myAddress.Street = "Shevchenko";
+            myAddress.House = "Loud";
+            myAddress.Apartment = "Cozy";
+
+            Console.WriteLine($"Index: {myAddress.Index}");
+            Console.WriteLine($"Country: {myAddress.Country}");
+            Console.WriteLine($"City: {myAddress.City}");
+            Console.WriteLine($"Street: {myAddress.Street}");
+            Console.WriteLine($"House: {myAddress.House}");
+            Console.WriteLine($"Apartment: {myAddress.Apartment}");
+        }
+    }
+}
---
+++ labi/converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace converter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Converter converter = new Converter(37.6, 40.5, 9.3);
+
+            Console.Write("Enter your money: ");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+
+            double userDollar = converter.ChangeUahInUsd(userInput);
+            double userEuro = converter.ChangeUahInEuro(userInput);
+            double userPln = converter.ChangeUahInPln(userInput);
+            
+            Console.WriteLine($"{userInput} UAH - {userDollar} USD");
+            Console.WriteLine($"{userInput} UAH - {userEuro} EURO");
+            Console.WriteLine($"{userInput} UAH - {userPln} PLN");
+        }
+    }
+}
