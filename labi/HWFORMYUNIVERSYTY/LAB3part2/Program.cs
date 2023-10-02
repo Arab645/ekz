41926e1c61cb732465852b200613ecf0e964a305
Arab645 <yasinski0202@gmail.com>
2023-10-02 23:33:34 +0300

    push first part of third lab

--- labi/HWFORMYUNIVERSYTY/LAB3part2/Program.cs
+++ labi/HWFORMYUNIVERSYTY/LAB3part2/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace LAB3part2
+﻿using System;
+
+namespace LAB3part2
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(" я не доделал еще эту часть лабораторной , но я сделаю в ближайшее время , просто по дедлайну работа згорает" +
+                              " и придется ее здать без одного задания , но я его запушу в скором времени");
         }
     }
 }
